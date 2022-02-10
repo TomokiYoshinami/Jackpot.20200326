60f157c3339f476d6724d471990ea9f3463ea8d1
Tomoki Yoshinami <united73@gmail.com>
2022-02-11 06:55:17 +0900

    !

---
+++ R/D/01_Analyze/D1115_IsHitA_glm Analyze Predict Full/script.R
@@ -0,0 +1,35 @@
+Sys.setenv(LANGUAGE = "CP932")
+
+print(paste0(Sys.time(),  " --- script.R Start ---"))
+
+args <- commandArgs(T)
+
+print(paste0("args=", args))
+print(paste0("args[1]=", args[1]))
+print(paste0("args[2]=", args[2]))
+print(paste0("args[3]=", args[3]))
+print(paste0("args[4]=", args[4]))
+print(paste0("args[5]=", args[5]))
+print(paste0("args[6]=", args[6]))
+print(paste0("args[7]=", args[7]))
+
+analyzeVersion <- as.character(args[1])
+analyzeCategory <- as.character(args[2])
+analyzeType <- as.character(args[3])
+analyzeFamily <- as.character(args[4])
+analyzeFunc <- as.character(args[5])
+analyzeTrackTypeCd <- as.character(args[6])
+analyzeJyokenCd <- as.character(args[7])
+
+print(paste0("analyzeVersion=", analyzeVersion))
+print(paste0("analyzeType=", analyzeType))
+print(paste0("analyzeFamily=", analyzeFamily))
+print(paste0("analyzeFunc=", analyzeFunc))
+print(paste0("analyzeTrackTypeCd=", analyzeTrackTypeCd))
+print(paste0("analyzeJyokenCd=", analyzeJyokenCd))
+
+sourceName <- paste0(analyzeCategory, "_", analyzeFunc, ".R")
+print(paste0("source=", sourceName))
+source(sourceName)
+
+print(paste0(Sys.time(),  " --- script.R Finished ---"))
---
+++ R/D/01_Analyze/D1115_IsHitA_glm Analyze Predict Full/source_logic_analyze_lmer.R
@@ -0,0 +1,67 @@
+print(paste0(Sys.time(),  " --- source_logic_lmer.R Start ---"))
+
+anayze.results.glm <- lmer(
+
+AnalyzeExpectedValueF01Gen02.ExpectedValue
+
+~
+
++ 1
+
++ AnalyzeExpectedValueF01Gen02.PrevExpectedValue + (AnalyzeExpectedValueF01Gen02.PrevExpectedValue|AnalyzeExpectedValueF01Gen02.HansyokuNum)
++ AnalyzeExpectedValueF01Gen02.PrevHitRateA + (AnalyzeExpectedValueF01Gen02.PrevHitRateA|AnalyzeExpectedValueF01Gen02.HansyokuNum)
+
+# ,offset = log(RaceResultByHorseByTotal.RunCount)
+# ,offset = log(+ RaceHorse.BaTaijyu + 1) + log(Odds1ByWin.TotalBetCount + 1) + log(RaceResultByHorseByTotal.RunCount + 1)
+# ,offset = log(RaceHorse.BaTaijyu) + log(Odds1ByWin.TotalBetCount+1)
+# ,offset = log(Odds1ByWin.TotalBetCount)
+# ,offset = log(RaceResultByJockyByTrackType.RunCount+1)
+# ,offset = log(RaceHorse.BaTaijyu) + log(Odds1ByWin.TotalBetCount+1) + log(RaceResultByJockyByTrackType.RunCount+1)
+# ,offset = log(Odds1ByWin.TotalBetCount+1) + log(RaceResultByJockyByTrackType.RunCount+1)
+
+,control=lmerControl(
+# ,optimizer = "bobyqa" # Default
+# ,optimizer = "Nelder_Mead"
+# ,optimizer = "optimx", optCtrl=list(method="L-BFGS-B", maxiter=100000)
+# ,optimizer = "optimx", optCtrl=list(method="nlminb", maxiter=100000)
+# ,restart_edge = TRUE
+# ,boundary.tol = 1e-5
+# ,calc.derivs=TRUE
+# ,use.last.params=FALSE
+# ,sparseX = FALSE
+## input checking options
+# ,check.nobs.vs.rankZ = "ignore"
+# ,check.nobs.vs.nlev = "stop"
+# ,check.nobs.vs.nlev = "ignore" #Recoomend
+# ,check.nlev.gtreq.5 = "ignore"
+# ,check.nlev.gtr.1 = "stop"
+# ,check.nlev.gtr.1 = "ignore" #Recoomend
+# ,check.nobs.vs.nRE="stop"
+# ,check.nobs.vs.nRE="ignore" # Reccomend
+# ,check.rankX = c("message+drop.cols", "silent.drop.cols", "warn+drop.cols","stop.deficient", "ignore")
+# ,check.scaleX = c("warning","stop","silent.rescale","message+rescale","warn+rescale","ignore")
+# ,check.formula.LHS = "stop"
+## convergence checking options
+# ,check.conv.grad = .makeCC("warning", tol = 2e-3, relTol = NULL)
+# ,check.conv.singular = .makeCC(action = "ignore",  tol = 1e-4)
+# ,check.conv.hess = .makeCC(action = "warning", tol = 1e-6)
+## ,optimizer args
+# ,optCtrl = list(maxfun=1000, maxiter=1000)
+)
+# ,family = "poisson"
+# ,family = "binomial"
+# ,family = "gaussian"
+#
+# ,method = "REML"
+# ,method = "ML"
+# ,method = "Laplace"
+# ,method = "AGQ" 	#Not Implement
+#
+# ,REML = TRUE # Default
+# ,REML =FALSE
+#
+,data=data
+# ,data = subset(data, Race.IsAnalyzeNormalPast == 1)
+)
+
+print(paste0(Sys.time(),  " --- source_logic_lmer.R Finished ---"))
