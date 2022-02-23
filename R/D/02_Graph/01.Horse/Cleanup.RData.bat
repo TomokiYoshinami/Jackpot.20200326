7459fe90b65fcb82f92895a571687d01c45529cc
Tomoki Yoshinami <united73@gmail.com>
2022-02-23 22:08:46 +0900

    !

---
+++ R/D/02_Graph/01.Horse/Cleanup.RData.bat
@@ -0,0 +1,7 @@
+rem @ECHO OFF
+REM del /s *.log
+REM del /s *.pdf
+del /s *.rdata
+del /s *.rdatatmp
+
+PAUSE
---
+++ R/D/02_Graph/01.Predict/!Graph_Predict.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+ECHO %Date% %Time% --- Started ---
+ECHO %CD%
+
+ECHO ********** Type00 **********
+Rscript --slave --vanilla --encoding=utf-8 scriptGraph.R "D1115" "Graph" "Predict" 0 "0"
+
+REM PAUSE
+
+REM R CMD BATCH --slave --vanilla --encoding=utf-8 script.R D1115 analyze Odds IsHitA glm 1 1
+REM Rscript --slave --vanilla --encoding=utf-8 script.R D1115 analyze Odds IsHitA glm 1 1
+
+REM CMD BATCHはすべて大文字
+REM R CMD BATCH --slave --vanilla ' --encoding=utf-8 script.R D1115 analyze Odds IsHitA glm 1 1
+REM OR
+REM Rscript scriptfile arg1 arg2 arg3
+ 
+ECHO %Date% %Time% --- Finished ---
+
+pause
